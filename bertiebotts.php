8a596a7c5461d4e9b290554e29ccbf496b2b56c0
1cec0ld <1cew1r3@gmail.com>
2016-03-28 07:21:55 -0700

    load beans based on which game is called; begin processing function

--- bertiebotts.php
+++ bertiebotts.php
@@ -1,14 +1,24 @@
 <?php
+//sunflower plains 4480 -1626
 /*House 0.6.26 Crest 1.2.24   Liver  brown*/
 /*Get from each 0.8.18   Chocolate  brown*/
 
-if($beanResult=$db->query('SELECT * FROM `myPHPbeans`')){
+function loadBeans($tableID){
     $beans=array();
+    $beanResult=debugQuery('SELECT * FROM `'.$tableID.'Beans`');
     while($beans[]=$beanResult->fetch_object()){
     }
-} else {
-    die('SOMETHING BROKE!<br>'.$db->error);
+    return $beans;
 }
-
+function processBean($beanObject,$username,$gameid){
+    //do the action to the database based on the bean
+        //if its a money type
+        //else if its a goto
+        //else its a getoutofjail
+    //tell the user what happened
+        //if its a bad bean, border broken
+        //else border solid
+        //set color to bean color
+}
 
 ?>
