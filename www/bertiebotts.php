adad57cdfe900da2b9a823674351dc1623f71977
1cec0ld <1cew1r3@gmail.com>
2016-03-28 08:30:13 -0700

    bean functionality version 1

--- www/bertiebotts.php
+++ www/bertiebotts.php
@@ -5,19 +5,34 @@
 function loadBeans($tableID){
     $beans=array();
     $beanResult=debugQuery('SELECT * FROM `'.$tableID.'Beans`');
-    while($beans[]=$beanResult->fetch_object()){
-    }
+        while($beans[]=$beanResult->fetch_object()){
+        } array_pop($beans);
     return $beans;
 }
 function processBean($beanObject,$username,$gameid){
-    //do the action to the database based on the bean
-        //if its a money type
-        //else if its a goto
-        //else its a getoutofjail
-    //tell the user what happened
-        //if its a bad bean, border broken
-        //else border solid
-        //set color to bean color
+    $SELECTResult=debugQuery('SELECT `money` FROM `myPHPusers` WHERE `username`="'.$username.'"');
+    if(is_numeric($beanObject->value)){
+        $value=intval($beanObject->value);
+        $newMoney=$SELECTResult->fetch_object()->money+$value;
+        printBean($value>0,'You '.($value<=0?'lost':'gained').' '.printHPMoney($value).'!',$beanObject);
+        return $newMoney;
+    } else if($beanObject->value=="GotoGo"){
+        printBean(true,'You Get a Letter to Hogwarts! This does nothing yet...',$beanObject);
+    } else if($beanObject->value=="GotoJail"){
+        printBean(false,'Now Go To Azkaban! This does nothing yet...',$beanObject);
+    } else {
+        debugQuery('UPDATE `'.$gameid.'Beans` SET `owner`="'.$username.'" WHERE `name`="'.$beanObject->name.'"');
+        printBean(true,'You found a Get Out of Azaban Free Card! This does nothing yet...',$beanObject);
+    }
+    return $SELECTResult->fetch_object()->money;
+}
+function printBean($good,$effect,$beanObject){
+    if($good){
+        $border='solid';
+    } else {
+        $border='dashed';
+    }
+    echo '<span style="border: 3px '.$border.' '.$beanObject->color.'"><strong> You ate a bean! It tasted like '.$beanObject->name.'! '.$effect.'</strong></span><br>';
 }
 
 ?>
