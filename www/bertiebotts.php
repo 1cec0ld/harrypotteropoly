9c16ac90bed7b2585d4588613eb71b567570b434
1cec0ld <1cew1r3@gmail.com>
2016-04-04 12:22:53 -0700

    Work on property support, first attempt to do all updates in functions

--- www/bertiebotts.php
+++ www/bertiebotts.php
@@ -14,7 +14,7 @@ function processBean($beanObject,$username,$gameid){
         $value=intval($beanObject->value);
         $newMoney=$SELECTResult->fetch_object()->money+$value;
         printBean($value>0,'You '.($value<=0?'lost':'gained').' '.printHPMoney($value).'!',$beanObject);
-        return $newMoney;
+        debugQuery('UPDATE `myPHPusers` SET `money`='.$newMoney.' WHERE `username`="'.$username.'"');
     } else if($beanObject->value=="GotoGo"){
         printBean(true,'You Get a Letter to Hogwarts! This does nothing yet...',$beanObject);
     } else if($beanObject->value=="GotoJail"){
@@ -29,12 +29,11 @@ function processBean($beanObject,$username,$gameid){
         }
         $newMoney=$SELECTResult->fetch_object()->money+$avar;
         printBean(true,'You coerce others into paying you 8 Sickles 18 Knuts!',$beanObject);
-        return $newMoney;
+        debugQuery('UPDATE `myPHPusers` SET `money`='.$newMoney.' WHERE `username`="'.$username.'"');
     } else {
         debugQuery('UPDATE `'.$gameid.'Beans` SET `owner`="'.$username.'" WHERE `name`="'.$beanObject->name.'"');
         printBean(true,'You found a Get Out of Azaban Free Card! This does nothing yet...',$beanObject);
     }
-    return $SELECTResult->fetch_object()->money;
 }
 function printBean($good,$effect,$beanObject){
     if($good){
